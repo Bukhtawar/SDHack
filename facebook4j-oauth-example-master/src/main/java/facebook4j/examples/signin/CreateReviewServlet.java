1cc5ce11cd3901b63a7b34bba4a4ea5f349958fc
varunkatiyar <varunkatiyar@L-6WWH32.(none)>
2015-09-04 19:30:23 +0530

    asdf

--- facebook4j-oauth-example-master/src/main/java/facebook4j/examples/signin/CreateReviewServlet.java
+++ facebook4j-oauth-example-master/src/main/java/facebook4j/examples/signin/CreateReviewServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.snapdeal.base.audit.annotation.AuditableClass;
 import com.snapdeal.base.exception.SnapdealWSException;
 import com.snapdeal.reviews.client.api.ReviewClientService;
 import com.snapdeal.reviews.client.factory.ReviewClientFactory;
@@ -18,6 +17,7 @@ import com.snapdeal.reviews.commons.OpinionBo;
 import com.snapdeal.reviews.commons.UserReviewsInfo;
 import com.snapdeal.reviews.commons.dto.ReviewRequest;
 import com.snapdeal.reviews.commons.dto.wrapper.CreateReviewRequest;
+import com.snapdeal.reviews.commons.dto.wrapper.CreateReviewResponse;
 
 public class CreateReviewServlet  extends HttpServlet {
 	
@@ -36,23 +36,24 @@ public class CreateReviewServlet  extends HttpServlet {
 			throws ServletException, IOException {
 		
 		Map<ConfigurationParams, String> configParams = new HashMap<ConfigurationParams, String>();
-		configParams.put(ConfigurationParams.BASE_URL, "http://10.1.28.14:8080/reviews-api");
+		configParams.put(ConfigurationParams.BASE_URL, "http://169.254.7.82:8080/reviews-api");
 		ReviewClientFactory.init(configParams);
 		ReviewClientService client = ReviewClientFactory.getClient();
 		
-		String headline = request.getParameter("headline");
 		CreateReviewRequest createReviewRequest = new CreateReviewRequest();
 		ReviewRequest reviewRequest = new ReviewRequest();
-		reviewRequest.setComments("");
-		reviewRequest.setHeadline("");
-		reviewRequest.setProductId("");
-		reviewRequest.setRating(3);
+		reviewRequest.setComments(request.getParameter("experience_value"));
+		reviewRequest.setHeadline(request.getParameter("headline_value"));
+		reviewRequest.setProductId("9876");
+		reviewRequest.setRating(Integer.parseInt(request.getParameter("reviewrating")));
 		reviewRequest.setRecommended(OpinionBo.YES);
 		reviewRequest.setUserReviewsInfo(new UserReviewsInfo("user", "nickname", Boolean.TRUE, 0));
 		createReviewRequest.setReviewRequest(reviewRequest);
 		
 		try {
-			client.createReview(createReviewRequest);
+			CreateReviewResponse reviewResponse = client.createReview(createReviewRequest);
+			String reviewId = reviewResponse.getReviewResponse().getId();
+			System.out.println(reviewId);
 		} catch (SnapdealWSException e) {
 			e.printStackTrace();
 		}
